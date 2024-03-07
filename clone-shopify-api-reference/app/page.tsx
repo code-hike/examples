79ddd7c735af22ee432184d05a280112522bd59d
Rodrigo Pombo <pombopombopombo@gmail.com>
2024-03-07 20:38:30 +0100

    Update home

---
+++ clone-shopify-api-reference/app/page.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link"
+
+export default function Page() {
+  return (
+    <main className="prose prose-invert mx-auto max-w-2xl mt-36">
+      <h1>Shopify API Reference Clone</h1>
+      <p>
+        Clone of{" "}
+        <a
+          href="https://shopify.dev/docs/api/admin-rest/2024-01/resources/order"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Shopify API Reference
+        </a>
+        , built with Code Hike v1.
+      </p>
+      Pages:
+      <ul>
+        <li>
+          <Link href="/order">Order</Link>
+        </li>
+        <li>
+          <Link href="/product">Product</Link>
+        </li>
+      </ul>
+      <p>
+        Find the{" "}
+        <a
+          href="https://github.com/code-hike/examples/tree/main/clone-shopify-api-reference"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          source code on GitHub
+        </a>
+        . For more information, see the{" "}
+        <a
+          href="https://v1.codehike.org/docs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Code Hike v1 docs
+        </a>
+        .
+      </p>
+    </main>
+  )
+}
